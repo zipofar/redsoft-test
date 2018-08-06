922e414bce4aa6f2a97358459d25f53ec6b92d02
Zipofar Ago <admin@zipofar.ru>
2018-08-06 17:59:41 +0300

    Fix tests MProduct

--- __tests__/MProductTest.php
+++ __tests__/MProductTest.php
@@ -23,13 +23,13 @@ class MProductTest extends TestCase
 
     public function getDataSet()
     {
-        $this->product = new MProduct();
-
         return $this->createFlatXMLDataSet(dirname(__FILE__).'/__fixtures__/testdata.xml');
     }
 
     public function testGetById()
     {
+        $product = new MProduct();
+
         $expected = [
             'id' => '1',
             'name' => 'FoodVegRedSour',
@@ -38,13 +38,15 @@ class MProductTest extends TestCase
             'brand' => 'Russia Kolhoz'
         ];
         
-        $this->assertEquals($expected, $this->product->getById(1));
+        $this->assertEquals($expected, $product->getById(1));
 
-        $this->assertEquals([], $this->product->getById(1000));
+        $this->assertEquals([], $product->getById(1000));
     }
 
     public function testGetBySubStrName()
     {
+        $product = new MProduct();
+
         $expectedFirstItem = [
             "id" => "8",
             "name" => "FoodFruitGreenSweet",
@@ -53,14 +55,26 @@ class MProductTest extends TestCase
             "brand" => "Petrovna"
         ];
 
-        $products = $this->product->getBySubStrName('sweet');
+        $products = $product->getBySubStrName('sweet');
 
         $this->assertEquals($expectedFirstItem, $products[0]);
         $this->assertEquals(4, count($products));
+
+        // Enable OFFSET = 1
+        $products = $product->getBySubStrName('sweet', 1);
+        $this->assertEquals(3, count($products));
+
+        // Enable LIMIT
+        $product2 = new MProduct(['limit' => 1]);
+        $products = $product2->getBySubStrName('sweet');
+
+        $this->assertEquals(1, count($products));
     }
 
     public function testGetByBrand()
     {
+        $product = new MProduct();
+
         $firstBrand = [
             "id" => "5",
             "name" => "FoodFruitRedSour",
@@ -82,17 +96,63 @@ class MProductTest extends TestCase
         $multyBrand[0] = $firstBrand;
         $multyBrand[1] = $secondBrand;
 
-        $this->assertEquals($singleBrand, $this->product->getByBrand("dacha"));
-        $this->assertEquals($multyBrand, $this->product->getByBrand("dacha+country"));
+        $this->assertEquals($singleBrand, $product->getByBrand("dacha"));
+        $this->assertEquals($multyBrand, $product->getByBrand("dacha+country"));
+
+        // Enable OFFSET = 1
+        $res1 = $product->getByBrand("Russia Kolhoz", 1);
+        $this->assertEquals(1, count($res1));
+
+        // Enable LIMIT
+        $product2 = new MProduct(['limit' => 1]);
+        $res2 = $product2->getByBrand("dacha+country");
+        $this->assertEquals(1, count($res2));
     }
 
     public function testGetBySection()
     {
-    
+        $product = new MProduct();
+        $expected = [
+            0 => ["id" => "5", "name" => "FoodFruitRedSour", "availability" =>"1", "price" => "1.50", "brand" => "Dacha"],
+            1 => ["id" => "6", "name" => "FoodFruitRedSweet", "availability" => "1", "price" => "1.50", "brand" => "Polsky"],
+            2 => ["id" => "1", "name" => "FoodVegRedSour", "availability" => "1", "price" => "1.99", "brand" => "Russia Kolhoz"],
+            3 => ["id" => "2", "name" => "FoodVegRedSweet", "availability" => "1", "price" => "1.50", "brand" => "Gruzin"]
+        ];
+
+        $this->assertEquals($expected, $product->getBySection('red'));
+
+        // Enable OFFSET = 1
+        $res1 = $product->getBySection('red', 1);
+        $this->assertEquals(3, count($res1));
+
+        // Enable LIMIT
+        $product2 = new MProduct(['limit' => 1]);
+        $this->assertEquals([$expected[0]], $product2->getBySection('red'));
     }
 
     public function testGetBySections()
     {
-    
+        $product = new MProduct();
+
+        $expected1 = [
+            0 => ["id" => "6", "name" => "FoodFruitRedSweet", "availability" => "1", "price" => "1.50", "brand" => "Polsky"]
+        ];
+
+        $this->assertEquals($expected1, $product->getBySections('food>>fruit>>red>>sweet'));
+
+        $res1 = $product->getBySections('food>>fruit>>red');
+        $this->assertEquals(2, count($res1));
+
+        $res2 = $product->getBySections('food');
+        $this->assertEquals(8, count($res2));
+
+        // Enable OFFSET = 1
+        $res3 = $product->getBySections('food>>fruit>>red', 1);
+        $this->assertEquals(1, count($res3));
+
+        // Enable LIMIT
+        $product2 = new MProduct(['limit' => 1]);
+        $res4 = $product2->getBySections('food>>fruit');
+        $this->assertEquals(1, count($res4));
     }
 }
