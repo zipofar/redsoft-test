ee18724f3ca5ca43e9af81fb0550e86ef7a57acb
ingprog <zipofar@gmail.com>
2018-08-15 20:05:48 +0300

    Add ansible

--- mysql/docker-entrypoint-initdb.d/01_create_testdb.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `redsoft`;
-CREATE DATABASE IF NOT EXISTS `redsoft_test`;
-
-CREATE USER 'redsoft' IDENTIFIED BY 'redsoft';
-GRANT ALL ON redsoft.* TO 'redsoft';
-GRANT ALL ON redsoft_test.* TO 'redsoft';
